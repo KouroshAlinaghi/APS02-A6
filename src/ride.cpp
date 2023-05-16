ad4f28034df791cd5cffa6761c192251330424c9
Kourosh Alinaghi <kouroshalinaghi@gmail.com>
2023-05-16 14:26:28 +0330

    shas mizanan be khoda

--- src/ride.cpp
+++ src/ride.cpp
@@ -5,7 +5,7 @@ int Ride::get_distance() {
 }
 
 int Ride::get_minutes() {
-    return (end_time - start_time) / 60;
+    return (end_time - start_time + 1) / 60;
 }
 
 Timestamp Ride::get_start_time() {
